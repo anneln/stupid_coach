8e76decff0dc8d50ab747f267904a14de944d3ce
anneln <ahbricet@gmail.com>
2014-11-24 15:00:13 +0100

    ok stupid coach rails

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,24 @@
+class CoachingController < ApplicationController
+  def answer
+    @query = params[:query]
+    @answer = coach_answer(@query)
+  end
+
+  def ask
+
+  end
+end
+
+def coach_answer(your_message)
+  # TODO: return coach answer to your_message
+  if your_message.downcase == "i am going to work right now!"
+    ""
+  elsif your_message.end_with?("?")
+    "Silly question, get dressed and go to work!"
+  else
+    "I don't care, get dressed and go to work!"
+  end
+end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'coaching#ask'
+  get 'answer', to: 'coaching#answer'
+  get 'ask', to: 'coaching#ask', as: "ask"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
